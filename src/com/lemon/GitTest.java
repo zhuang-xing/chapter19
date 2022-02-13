c5fdee5b720150c865c0d0e2d84a5e2a9bd55fc4
软柠柠吖 <248178734@qq.com>
2022-02-13 19:40:07 +0800

    hot-fix commit

--- src/com/lemon/GitTest.java
+++ src/com/lemon/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("hello Git!");
 		System.out.println("hello Git3!");
+		System.out.println("hello Git4! hot-fix");
 	}
 }
