f98a251f51f0e204eb4dc11f86e216a9670808aa
软柠柠吖 <248178734@qq.com>
2022-02-13 19:47:33 +0800

    hot-fix test

--- src/com/lemon/GitTest.java
+++ src/com/lemon/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
 		System.out.println("hello Git!");
 		System.out.println("hello Git3!");
 		System.out.println("hello Git4! hot-fix");
+		System.out.println("hot-fix test");
 	}
 }
